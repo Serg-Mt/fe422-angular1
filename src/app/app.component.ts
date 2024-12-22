b6c6e050b40be65c803834f58f52056d3aee3cb4
SergMt <66214453+Serg-Mt@users.noreply.github.com>
2024-12-22 13:56:39 +0400

    22 декабря

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { MainComponent } from './main/main.component';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [ MainComponent],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'prj1-tl';
+  nav = [
+    { href: '/', title: 'Home1' },
+    { href: '/les1', title: 'les 1 template lang' },
+    { href: '/les2', title: 'les 2 components' }
+  ];
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent, ParentComponent } from './small.component';
+import { MainComponent } from './main/main.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'les1', component: MainComponent },
+  { path: 'les2', component: ParentComponent }
+];
